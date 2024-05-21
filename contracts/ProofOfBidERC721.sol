343b0c90ce3e90f76e05b3374eef78b794a4e9f7
Ly Ho Van <hovanlybkit@gmail.com>
2024-05-21 21:51:18 +0700

    Add function

---
+++ contracts/ProofOfBidERC721.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ProofOfBidERC721 is ERC721 {
+    uint public currentTokenId = 0;
+    struct Bid {
+        address bidder;
+        uint tokenId;
+        uint amount;
+    }
+    mapping(uint => Bid) public bids;
+
+    address public owner;
+    address public mintOperator;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+    modifier onlyMintOperator() {
+        require(
+            msg.sender == mintOperator,
+            "Only mint owner can call this function"
+        );
+        _;
+    }
+
+    function setMintOwner(address _mintOwner) external onlyOwner {
+        mintOperator = _mintOwner;
+    }
+
+    constructor() ERC721("ProofOfBidNFT", "T4ENFT") {
+        owner = msg.sender;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+        // TODO iamge url
+        return "";
+    }
+
+    function mintProof(
+        address recipient,
+        uint bidTokenId,
+        uint bitAmout
+    ) external onlyMintOperator returns (uint) {
+        currentTokenId += 1;
+        uint tokenId = currentTokenId;
+        _safeMint(recipient, tokenId);
+        bids[tokenId] = Bid(recipient, bidTokenId, bitAmout);
+        return tokenId;
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+import { resolve } from "path";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    inco: {
+      accounts: [
+        process.env.PRIVATE_KEY_1 as string
+      ],
+      chainId: 9090,
+      url: "https://testnet.inco.org",
+    }
+  },
 };
 
 export default config;
